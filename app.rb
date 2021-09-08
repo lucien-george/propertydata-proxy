6bb1ca14d16d02130198cfa582739a62581ee413
Lucien George <luciengeorge95@gmail.com>
2021-09-08 16:56:48 +0100

    fixed error with space

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ get "/*" do
   path = params[:splat].first
   params.delete('splat')
   params_key = params.sort_by { |k, _| k }.map { |k, v| [k, v].join(':') }.join(':')
-  query = params.sort_by { |k, _| k }.map { |k, v| "#{k}=#{v}" }.join('&')
+  query = params.sort_by { |k, _| k }.map { |k, v| "#{k}=#{v.gsub(' ', '')}" }.join('&')
   key = "#{Date.today.to_s}:#{path}:#{params_key}"
   settings.cache.fetch(key) do
     url = "https://api.propertydata.co.uk/#{path}?key=#{ENV['PROPERTY_TOKEN']}&#{query}"
