32427f47d2eaa82ab8ac5c80d3eab4d845b705ab
Lucien George <luciengeorge95@gmail.com>
2021-09-08 12:38:04 +0100

    finished proxy for propertyapi

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'date'
 require 'sinatra'
 require 'redis-sinatra'
 require 'http'
+require 'pry'
 
 register Sinatra::Cache
 
@@ -12,9 +13,12 @@ end
 get "/*" do
   content_type 'application/json'
   path = params[:splat].first
-  key = "#{Date.today.to_s}:#{path}"
+  params.delete('splat')
+  params_key = params.sort_by { |k, _| k }.map { |k, v| [k, v].join(':') }.join(':')
+  query = params.sort_by { |k, _| k }.map { |k, v| "#{k}=#{v}" }.join('&')
+  key = "#{Date.today.to_s}:#{path}:#{params_key}"
   settings.cache.fetch(key) do
-    url = "https://api.themoviedb.org/3/#{path}?api_key=#{ENV['TMDB_API_KEY']}"
+    url = "https://api.propertydata.co.uk/#{path}?key=#{ENV['PROPERTY_TOKEN']}&#{query}"
     HTTP.get(url).body.to_s
   end
 end
