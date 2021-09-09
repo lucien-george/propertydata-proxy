e887cf42950d7e63e8fd9516da971b397f96cfd0
Lucien George <luciengeorge95@gmail.com>
2021-09-09 13:05:50 +0100

    removed api key from prox (needs to be set by usery

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ get "/*" do
   query = params.sort_by { |k, _| k }.map { |k, v| "#{k}=#{v.gsub(' ', '')}" }.join('&')
   key = "#{Date.today.to_s}:#{path}:#{params_key}"
   settings.cache.fetch(key) do
-    url = "https://api.propertydata.co.uk/#{path}?key=#{ENV['PROPERTY_TOKEN']}&#{query}"
+    url = "https://api.propertydata.co.uk/#{path}?#{query}"
     HTTP.get(url).body.to_s
   end
 end
